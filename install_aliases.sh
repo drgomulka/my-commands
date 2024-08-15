00fd4d9be449bf3e247215c91e50b34963b1d05b
drgomulka <80592261+drgomulka@users.noreply.github.com>
2024-08-15 20:44:22 +0200

    BIG Update install_aliases.sh

--- install_aliases.sh
+++ install_aliases.sh
@@ -1,77 +1,66 @@
 #!/usr/bin/sh
-echo -e "\e[31m# simple shell script to add the my-commands alias to the alias file\e[0m"
-echo "# ~/.bashrc.d/         dir will be created if it does not exist"
-echo "# ~/.bashrc.d/aliases file will be created if it does not exist"
-echo "# ~/.bash_aliases file will be created if it does not exist"
+echo "\e[33m# simple shell script to add the my-commands alias to the alias file\e[0m"
+echo '# ~/.bashrc.d/         dir will be created if it does not exist and system is Fedora style'
+echo '# ~/.bashrc.d/aliases file will be created if it does not exist and system is Fedora style'
+echo '# ~/.bash_aliases file will be created if it does not exist and system is Ubuntu style'
+
+string_fzf="alias my-fzf-commands='chosen_command=\$(cat ~/.dotfiles/my-commands/commands_list.txt | fzf ) ; eval \$chosen_command '"
+string_sel="alias my-sel-commands='PS3=\"Select command: \"; mapfile -t items < ~/.dotfiles/my-commands/commands_list.txt; select item in \"\${items[@]}\"; do eval \$item;break; done'"  >> ~/.bash_aliases
 
 
 fedora_style() {
 # for Fedora
-  if grep -Fxq  $1    ~/.bashrc.d/aliases
+
+    echo '. mkdir -p ~/.bashrc.d'
+    mkdir -p  ~/.bashrc.d
+    echo '. touch -a ~/.bashrc.d/aliases'
+    touch -a  ~/.bashrc.d/aliases
+
+  if grep -Fq  "my-commands"  ~/.bashrc.d/aliases
   then
-    echo ". alias found in file ~/.basrc.d/aliases; good, do nothing"
+    echo '. my-commands tag found in file ~/.bashrc.d/aliases; good, do nothing'
   else
-    echo "! alias not found in file ~/.basrc.d/aliases; write it in"
-    echo $1   >> ~/.bashrc.d/aliases
+    # the key part, adding the alias to the aliases file
+    echo '! alias not found in file ~/.bashrc.d/aliases; write it in'
+    echo '#### my-commands ####'   >> ~/.bashrc.d/aliases
+    echo "$string_fzf"   >> ~/.bashrc.d/aliases
+    echo "$string_sel"   >> ~/.bashrc.d/aliases
+    # echo "alias my-fzf-commands='chosen_command=\$(cat ~/.dotfiles/my-commands/commands_list.txt | fzf ) ; eval \$chosen_command '"   >> ~/.bashrc.d/aliases
+    # echo "alias my-sel-commands='PS3=\"Select command: \"; mapfile -t items < ~/.dotfiles/my-commands/commands_list.txt; select item in \"${items[@]}\"; do eval \$item;break; done'"  >> ~/.bashrc.d/aliases
   fi
-
-
 }
 
 ubuntu_style(){
+  # for Ubuntu style bash_aliases
+    echo '. touch -a ~/.bash_aliases'
+    touch -a ~/.bash_aliases
 
-
-# for Ubuntu
- if grep -Fxq  $1   ~/.bash_aliases
-  then
-    echo ". alias found in file  ~/.bash_aliases; good, do nothing"
-  else
-    echo "! alias not found in file ~/.bash_aliases; write it in"
-    echo $1   >> ~/.bash_aliases
+  if grep -Fq  "my-commands"   ~/.bash_aliases
+    then
+      echo '. my-commands tag found in file  ~/.bash_aliases; good, do nothing'
+    else
+      # the key part, adding the alias to the aliases file
+      echo '! alias not found in file ~/.bash_aliases; write it in'
+      echo '#### my-commands ####'   >> ~/.bash_aliases
+      echo "$string_fzf"   >> ~/.bash_aliases
+      echo "$string_sel"   >> ~/.bash_aliases
   fi
 }
-
-
 
 main_function() {
 
-  # making sure the dir for aliases exists
-  if [ -d ~/.bashrc.d ] ; then
-    echo ". directory ~/.bashrc.d         exists, good."
-  else
-    echo ". mkdir ~/.bashrc.d"
-    mkdir  ~/.bashrc.d
+  if grep -Fq  '~/.bash_aliases'   ~/.bashrc
+    then
+      echo '. .bashrc calls on .bash_aliases: this system is Ubuntu style'
+      ubuntu_style
+   fi
+
+  if grep -Fq  '~/.bashrc.d/aliases' ~/.bashrc
+    then
+      echo '. .bashrc calls on .bashrc.d/aliases: this system is Fedora style'
+      fedora_style
   fi
-
-  # making sure the aliases file exists for Fedora
-  if [ -f ~/.bashrc.d/aliases ]
-  then
-    echo ". file      ~/.bashrc.d/aliases exists, good."
-  else
-    echo ". touch  ~/.bashrc.d/aliases"
-    touch  ~/.bashrc.d/aliases
-  fi
-
-  # making sure the aliases file exists for Ubuntu
-  if [ -f ~/.bash_aliases ]
-  then
-    echo ". file      ~/.bash_aliases exists, good."
-  else
-    echo ". touch  ~/.bash_aliases"
-    touch  ~/.bash_aliases
-  fi
-
-  # the key part, adding the alias to the aliases file
-  line_to_add_to_aliases="alias my-commands='chosen_command=\$(cat ~/.dotfiles/my-commands/commands_list.txt | fzf ) ; eval \$chosen_command '"
-
-fedora_style "$line_to_add_to_aliases"
-
-ubuntu_style "$line_to_add_to_aliases"
-
 }
-
-
-
 
 #run the main function
 main_function
